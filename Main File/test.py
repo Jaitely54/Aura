4a2e33f8376d9a135c309bb3422d4a1a12cb83df
Kritish Jaitely <136069402+Jaitely54@users.noreply.github.com>
2024-02-02 21:21:03 +0530

    Update test.py

--- Main File/test.py
+++ Main File/test.py
@@ -16,8 +16,6 @@ import math
 import warnings
 warnings.simplefilter('ignore')
 
-
-
 #  Creating a function for the voice of the Assistant ------------>
 def speak(text):
     engine = pyttsx3.init()
@@ -72,61 +70,6 @@ def greet():
     speak(message)
 
 
-# def calculator_operation(query):
-#     if "calculate" in query:
-#         print("Sure, let's perform a calculation.")
-        
-#         # Extract relevant information from the user's query
-#         operation = ""
-#         numbers = []
-#         for word in query.split():
-#             if word.isdigit() or (word[0] == '-' and word[1:].isdigit()):
-#                 numbers.append(float(word))
-#             elif word.lower() in ["add", "plus"]:
-#                 operation = "add"
-#             elif word.lower() in ["subtract", "minus"]:
-#                 operation = "subtract"
-#             elif word.lower() in ["multiply", "times"]:
-#                 operation = "multiply"
-#             elif word.lower() in ["divide", "over"]:
-#                 operation = "divide"
-#             elif word.lower() == "square":
-#                 operation = "square"
-#             elif word.lower() in ["root", "sqrt"]:
-#                 operation = "sqrt"
-#             elif word.lower() in ["exponentiate", "power"]:
-#                 operation = "exponentiate"
-
-#         # Perform the requested calculation
-#         if operation == "add":
-#             result = sum(numbers)
-#         elif operation == "subtract":
-#             result = numbers[0] - sum(numbers[1:])
-#         elif operation == "multiply":
-#             result = 1
-#             for num in numbers:
-#                 result *= num
-#         elif operation == "divide":
-#             result = numbers[0]
-#             for num in numbers[1:]:
-#                 result /= num
-#         elif operation == "square":
-#             result = numbers[0] ** 2
-#         elif operation == "sqrt":
-#             result = numbers[0] ** 0.5
-#         elif operation == "exponentiate":
-#             result = numbers[0]
-#             for exponent in numbers[1:]:
-#                 result **= exponent
-#         else:
-#             return "I'm sorry, I couldn't understand the calculation request."
-
-#         return f"The result is {result:.2f}"
-    
-
-#     return None
-
-# calculator_result = calculator_operation()
 def currenttime():
     currenttime = datetime.datetime.now()
     time1 = currenttime.strftime("%I %M:%p")
